e648dabc4702c3b3641ad9c93222de32a0503f72
MusicalProf <krishon2006@gmail.com>
2020-12-02 13:12:29 -0600

    Assignment 6 & 9: Human OOP, WizardNinjaSamurai
    An assignment practicing the beginnings of OOP. Will be updated with additional classes and methods.

---
+++ Human/Human.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace Human
+{
+    public class Human
+    {
+        // Fields for Human
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+
+        // add a public "getter" property to access health
+        public int Health
+        {
+            get { return health; }
+        }
+        // Add a constructor that takes a value to set Name, and set the remaining fields to default values
+        public Human(string name)
+        {
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
+        // Add a constructor to assign custom values to all fields
+        public Human(string name, int str, int intel, int dex, int hp)
+        {
+            Name = name;
+            Strength = str;
+            Intelligence = intel;
+            Dexterity = dex;
+            health = hp;
+        }
+
+        // Build Attack method
+        public int Attack(Human target)
+        {
+            int damage = this.Strength * 5;
+            target.health -= damage;
+            Console.WriteLine($"{this.Name} attacked {target.Name} and did {damage} hitpoints of damage!!!\n{target.Name} has {target.health} hp remaining!");            
+            return target.health;
+        }
+    }
+}
---
+++ Human/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Human Ramar = new Human("Ra'mar");
+            Human Target = new Human("Tamster");
+
+            Ramar.Attack(Target);
+        }
+    }
+}
